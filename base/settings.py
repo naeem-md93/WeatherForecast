4de995880d893d0602a5f7ce7f32533824195404
Naeem Mohammadi <naeem.mohammadi1993@gmail.com>
2025-03-08 00:26:04 +0330

    fix allowed_hosts

--- base/settings.py
+++ base/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$cj%w(a@$h5_@f^fp)t4#zpx*g6ob#iufxwk-_ku!rh2k21+1v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 
 # Application definition
